332b580c62475a3112298cf5fea240c805d1a7e9
Rafael Galeno <rafaellazaro.souza@gmail.com>
2019-04-09 10:44:38 -0300

    First controller and Razor pages tests

--- SalesWebMvc/Controllers/HomeController.cs
+++ SalesWebMvc/Controllers/HomeController.cs
@@ -17,7 +17,8 @@ namespace SalesWebMvc.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Conheça nossa empresa!";
+            ViewData["Professor"] = "Rafael Galeno";
 
             return View();
         }
