721c4b0edf10c8372dee036c719ce54fbaf1d9a0
Rafael Galeno <rafaellazaro.souza@gmail.com>
2019-04-09 16:50:39 -0300

    criando formulario e inserindo dados

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -19,5 +19,11 @@ namespace SalesWebMvc.Services
         {
             return _context.Seller.ToList();//traz lista de vendedores do banco de dados. Operação sincrona
         }
+
+        public void Insert(Seller obj)
+        {
+            _context.Add(obj);
+            _context.SaveChanges();
+        }
     }
 }
